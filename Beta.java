1c9b39823e57f74f8b5ef062bd753adaf833ed9d
Vaishnavi Ahirrao <vaishnaviahirrao10497@gmail.com>
2022-11-14 19:40:50 +0530

    first challenge

---
+++ Beta.java
@@ -0,0 +1,14 @@
+ import java.util.Scanner;     
+public class Beta{
+     public static void main(String[] args) 
+	 { 
+	   Scanner t = new Scanner(System.in);
+   System.out.println("Enter two number");
+ int i = t.nextInt();
+  int j = t.nextInt();
+if(i<j)
+ System.out.println(j+" is greater ");
+ else
+System.out.println(i+" is greater");
+		}
+}
